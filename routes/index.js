e6f0b871bd7b7cf985fa5d303cfebe22b181267a
cucmakeit <cucmakeit@gmail.com>
2014-10-11 15:48:12 +0800

    完成首页

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
+router.get('/time', function(req, res){
+	res.send('The time is ' + new Date().toString());
+});
+
+router.get('/user/:username', function(req, res){
+	res.send('user: '+ req.params.username);
+});
 module.exports = router;
